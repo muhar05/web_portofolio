6724c27d9c4fc4a2553d7c8a8c0465481894b755
muhar05 <ferdiansyahmuh5@gmail.com>
2025-02-27 15:50:32 +0700

    fix bug

--- src/app/components/views/Home/page.tsx
+++ src/app/components/views/Home/page.tsx
@@ -60,7 +60,7 @@ const HomePage = () => {
           <TextGenerateEffect words={words} />
           <Link href="/link">
             <button className="shadow-[inset_0_0_0_2px_#616467] text-black px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200 mt-6">
-              Let's Get in Touch
+              Let&apos;s Get in Touch
             </button>
           </Link>
         </div>
